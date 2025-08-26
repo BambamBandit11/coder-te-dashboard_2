2940f4f287596b63d66bdc49f078c1ff43fb0acf
BambamBandit11 <peggy.e.mathison@gmail.com>
2025-08-26 16:23:58 -0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,23 +1,19 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-const authOptions = {
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
-  pages: {
-    signIn: '/auth/signin',
-    error: '/auth/error',
-  },
   callbacks: {
     async signIn({ user }) {
-      return user.email?.endsWith('@coder.com') || false
+      if (user.email?.endsWith('@coder.com')) {
+        return true
+      }
+      return false
     }
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-}
-
-export default NextAuth(authOptions)
+  }
+})
